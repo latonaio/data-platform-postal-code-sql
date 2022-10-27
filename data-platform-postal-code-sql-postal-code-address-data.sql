8034b1c039f6fc83fd7dda4aeb5c0ac0d7a03c2d
MiyuYokoyama0109 <miyu.y@latona.io>
2022-10-27 11:47:38 +0900

    release

--- data-platform-postal-code-sql-postal-code-address-data.sql
+++ data-platform-postal-code-sql-postal-code-address-data.sql
@@ -1,13 +1,18 @@
 CREATE TABLE `data_platform_postal_code_postal_code_address_data`
 (
-    `PostalCode`              varchar(10) NOT NULL,
-    `LocalRegion`             varchar(3) NOT NULL,
-    `Country`                 varchar(3) NOT NULL,
-    `Language`                varchar(2) NOT NULL,
-    `StreetName`              varchar(200) DEFAULT NULL,
-    `CityName`                varchar(200) DEFAULT NULL,
-    `AddressText`             varchar(300) DEFAULT NULL,
-    PRIMARY KEY (`PostalCode`, `LocalRegion`, `Country`, `Language`),
-    CONSTRAINT `DataPlatformPostalCodePostalCodeAddressData_fk` FOREIGN KEY (`PostalCode`, `LocalRegion`, `Country`) REFERENCES `data_platform_postal_code_postal_code_data` (`PostalCode`, `LocalRegion`, `Country`)
+    `PostalCode`                   varchar(10) NOT NULL,
+    `LocalRegion`                  varchar(3) NOT NULL,
+    `Country`                      varchar(3) NOT NULL,
+    `GlobalRegion`                 varchar(3) DEFAULT NULL,
+    `PostalCodeAddressDetailText`  varchar(200) DEFAULT NULL,
+    `CityName`                     varchar(200) DEFAULT NULL,
+    `Builiding`                    varchar(100) DEFAULT NULL,
+    `Floor`                        int(4) DEFAULT NULL,
+    `PostalCodeAddressTotalText`   varchar(300) DEFAULT NULL,
+
+    PRIMARY KEY (`PostalCode`, `LocalRegion`, `Country`),
+
+    CONSTRAINT `DataPlatformPostalCodePostalCodeAddressData_fk` FOREIGN KEY (`PostalCode`, `LocalRegion`, `Country`, `GlobalRegion`) REFERENCES `data_platform_postal_code_postal_code_data` (`PostalCode`, `LocalRegion`, `Country`, `GlobalRegion`)
+    
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
--- data-platform-postal-code-sql-postal-code-data.sql
+++ data-platform-postal-code-sql-postal-code-data.sql
@@ -3,6 +3,11 @@ CREATE TABLE `data_platform_postal_code_postal_code_data`
     `PostalCode`             varchar(10) NOT NULL,
     `LocalRegion`            varchar(3) NOT NULL,
     `Country`                varchar(3) NOT NULL,
+    `GlobalRegion`           varchar(3) NOT NULL,
+
     PRIMARY KEY (`PostalCode`, `LocalRegion`, `Country`)
+
+    CONSTRAINT `DataPlatformPostalCodePostalCodeDataLocalRegion_fk` FOREIGN KEY (`LocalRegion`, `Country`, `GlobalRegion`) REFERENCES `data_platform_local_region_local_region_data` (`LocalRegion`, `Country`, `GlobalRegion`)
+
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
